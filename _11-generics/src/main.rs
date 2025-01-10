954f9cf05de1d571a9909445175ad6604aca1393
albertalvin8080 <albertalvin99@gmail.com>
2025-01-10 15:02:53 -0300

    feat: mixup

--- _11-generics/src/main.rs
+++ _11-generics/src/main.rs
@@ -1,44 +1,35 @@
 #![allow(warnings)]
 
-struct Point<T> {
+#[derive(Debug)]
+struct Point<T, U> {
     x: T,
-    y: T,
+    y: U,
 }
 
-impl<T: std::ops::Add<Output = T> + std::ops::Sub<Output = T> + Copy> Point<T> {
-    pub fn new(x:T, y:T) -> Self {
-        Self {
-            x, y,
+impl<T, U> Point<T, U>
+where
+    T: Clone,
+{
+    pub fn mixup<V, W: Clone>(&self, other: &Point<V, W>) -> Point<T, W> {
+        Point {
+            x: self.x.clone(),
+            y: other.y.clone(),
         }
-    }
-
-    pub fn add(&self) -> T {
-        self.x + self.y
-    }
-
-    pub fn sub(&self) -> T {
-        self.x - self.y
-    }
-}
-
-// Only for <f64> types
-impl Point<f64> {
-    pub fn distance_from_origin(self: &Self) -> f64 {
-        (self.x.powi(2) + self.y.powi(2)).sqrt()
-    }
-    pub fn angle_with_x(&self) -> f64 {
-        self.y.atan2(self.x)
     }
 }
 
 fn main() {
-    let pf64 = Point::new(3.0, 4.0);
-    println!("{}", pf64.distance_from_origin());
-    println!("{}", pf64.angle_with_x());
-    println!("{}", pf64.add());
-    println!("{}", pf64.sub());
-    
-    let pu8 = Point::new(32 as u8, 12 as u8);
-    println!("{}", pu8.add());
-    println!("{}", pu8.sub());
+    let p1 = Point{
+        x: '愛',
+        y: "Franz",
+    };
+
+    let p2= Point{
+        x: 3 as u32,
+        y: 74 as f64,
+    };
+
+    let p3 = p1.mixup(&p2);
+
+    println!("{:?}", p3);
 }
